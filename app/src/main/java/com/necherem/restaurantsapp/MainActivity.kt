40314aea6c39ced9b09eb1a541e0e38dad43de2e
Kalu Chinecherem <kaluchinecherem8@gmail.com>
2023-06-09 02:10:29 +0100

    Initial commit

---
+++ app/src/main/java/com/necherem/restaurantsapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.necherem.restaurantsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.necherem.restaurantsapp.ui.theme.RestaurantsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RestaurantsAppTheme {
+                RestaurantsScreen()
+            }
+        }
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RestaurantsAppTheme {
+        RestaurantsScreen()
+    }
+}
